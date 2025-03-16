db2f1fb6c28b49fb21e40650c48d15a758af0f8e
Kimen <Kimentanm@gmail.com>
2025-03-16 17:23:51 +0800

    Update UPnPRegistry.swift

--- Source/UPnP/UPnPRegistry.swift
+++ Source/UPnP/UPnPRegistry.swift
@@ -84,7 +84,7 @@ public class UPnPRegistry {
     }
     
     public func startDiscovery(_ types: [String] = ["urn:schemas-upnp-org:device:MediaServer:1", "urn:linn-co-uk:device:Source:1", "urn:av-openhome-org:device:Source:1"]) throws {
-        let filteredTypes = types.filter { $0.contains(":device:") }
+        let filteredTypes = types.filter { $0.contains(":service:") }
         guard filteredTypes.count == types.count else {
             Logger.swiftUPnP.error("Only device types are discovered, service types will be discovered indirectly from the device description. Non-device types will be filtered.")
             return
